8157c9e04b3a578143137167c5409306ae12f1e0
Nathan Lee <nmlee@ksu.edu>
2023-04-26 17:54:04 -0500

    Fix procedure CommonMedicationByBreed

--- VetDatabase/dbo/Stored Procedures/CommonMedicationByBreed.sql
+++ VetDatabase/dbo/Stored Procedures/CommonMedicationByBreed.sql
@@ -1,53 +1,17 @@
-﻿--CREATE PROCEDURE CommonMedicationByBreed
---AS
-
---WITH breed_count_cte(PetID, BreedID, BreedCount) AS (
---    SELECT P.PetID, P.BreedID, COUNT(P.PetID) AS BreedCount
---    FROM Pet P
---    GROUP BY P.BreedID
---),
-
---med_cte(PetID, MedicationID, MedCount) AS (
---    SELECT PM.PetID, PM.MedicationID, COUNT(PM.PetID) OVER (PARTITION BY P.BreedID) AS MedCount
---    FROM PetMed PM
---    INNER JOIN Pet P ON P.PetID = PM.PetID
---)
-
---SELECT
---    RANK() OVER (PARTITION BY B.BreedID, MCTE.MedCount ORDER BY MCTE.MedCount DESC) AS Rank,
---    B.BreedName,
---    BCTE.BreedCount,
---    M.MedicationName AS MostCommonMedicationName,
---    COUNT(MCTE.MedCount) OVER (PARTITION BY B.BreedID, MCTE.MedCount) AS TotalPrescribed 
---FROM breed_count_cte BCTE
---    INNER JOIN med_cte MCTE ON MCTE.PetID = BCTE.PetID
---    INNER JOIN Medication M ON MCTE.MedicationID = M.MedicationID
---    INNER JOIN Breed B ON B.BreedID = BCTE.BreedID
-
---GO;
-
-CREATE PROCEDURE CommonMedicationByBreed
+﻿CREATE PROCEDURE CommonMedicationByBreed
 AS
-
-WITH breed_count_cte(PetID, BreedID, BreedCount) AS (
-    SELECT P.PetID, P.BreedID, COUNT(P.PetID) AS BreedCount
-    FROM Pet P
-    GROUP BY P.BreedID, P.PetID
-),
-
-med_cte(PetID, MedicationID, MedCount) AS (
-    SELECT PM.PetID, PM.MedicationID, COUNT(PM.PetID) OVER (PARTITION BY P.BreedID) AS MedCount
+WITH med_cte(BreedID, MedicationID, MedCount) AS (
+    SELECT P.BreedID, PM.MedicationID, COUNT(*) AS MedCount
     FROM PetMed PM
     INNER JOIN Pet P ON P.PetID = PM.PetID
+	GROUP BY P.BreedID, PM.MedicationID
 )
 
 SELECT
-    RANK() OVER (PARTITION BY BCTE.BreedID, MCTE.MedCount ORDER BY MCTE.MedCount DESC) AS Rank,
+    RANK() OVER (ORDER BY MCTE.MedCount DESC) AS [Rank],
     B.BreedName,
-    BCTE.BreedCount,
     M.MedicationName AS MostCommonMedicationName,
-    COUNT(MCTE.MedCount) OVER (PARTITION BY BCTE.BreedID, MCTE.MedCount) AS TotalPrescribed 
-FROM breed_count_cte BCTE
-INNER JOIN med_cte MCTE ON MCTE.PetID = BCTE.PetID
+	MCTE.MedCount AS TotalPrescribed
+FROM med_cte MCTE
 INNER JOIN Medication M ON MCTE.MedicationID = M.MedicationID
-INNER JOIN Breed B ON B.BreedID = BCTE.BreedID
+INNER JOIN Breed B ON B.BreedID = MCTE.BreedID
